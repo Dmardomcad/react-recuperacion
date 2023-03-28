17f6ba911a8bc733df5a70b1fbb22dd076e47263
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-03-28 12:55:25 +0200

    styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { router } from './router'
 import { RouterProvider } from 'react-router-dom'
-import './index.css'
+import './sass/main.sass'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
