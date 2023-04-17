fcec1c48b9dd7e90dbb8218f11959678dee8612c
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-04-17 23:50:44 +0200

    contacto

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <section>
+        <h3>CONTACTE CON NOSOTROS</h3>
+        <form>
+          <ul>
+            <li>
+              <label htmlFor="nombre">Nombre: </label>
+              <input type="text" placeholder='Nombre' />
+            </li>
+            <li>
+              <label htmlFor="email">Email: </label>
+              <input type="text" placeholder='Email' />
+            </li>
+            <li>
+              <textarea className="espacio-mensaje" cols="30" rows="10" placeholder='Por favor introduzca su mensaje'></textarea>
+              <button className='btn-registro' type='submit'>Enviar</button>
+            </li>
+          </ul>
+        </form>
+      </section>
+    </>
   )
 }
 
